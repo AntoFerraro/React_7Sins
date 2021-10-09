dd4dddf25d7d661ee82839be5d42e043e81252c3
AntoFerraro <antodibe_9@hotmail.com>
2021-10-09 19:11:45 -0300

    clase 2

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//1)Tiene que estar la variable React en scope
+import React from "react"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//2)Tiene que estar la variable ReactDOM en scope
+import ReactDOM from "react-dom"
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//3)Tengo que tener un elemento
+const Elemento = () => <p> Hola Mundo! </p>
+
+//4) Tengo que renderizar el Elemento, solo pasa una vez y nunca mas
+ReactDOM.render(<Elemento/>, document.getElementById("root")); //que quiero renderizar y donde
