11ecaa5b2b60b0e9880013dab94b848c00efe2df
AntoFerraro <antodibe_9@hotmail.com>
2021-10-16 02:46:46 -0300

    clase 3

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+const NavBar = () => {
+return (
+    <header className="header">
+        <nav className="header__nav">
+            <div className="header__div">
+                <h2 className="header__titulo"> 7SINS </h2>
+            </div>
+            <ul className="header__lista">                
+                <li > <a className="header__link" href=""> Productos </a> </li>
+                <li> <a className="header__link" href=""> Contactos </a> </li>                
+            </ul>
+        </nav>
+    </header>
+)
+}
+
+export default NavBar
--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
-//1)Tiene que estar la variable React en scope
 import React from "react"
-
-//2)Tiene que estar la variable ReactDOM en scope
 import ReactDOM from "react-dom"
 
-//3)Tengo que tener un elemento
-const Elemento = () => <p> Hola Mundo! </p>
-
-//4) Tengo que renderizar el Elemento, solo pasa una vez y nunca mas
-ReactDOM.render(<Elemento/>, document.getElementById("root")); //que quiero renderizar y donde
+import NavBar from "./components/NavBar"
+import "./styles.css"
+ReactDOM.render(<NavBar/>, document.getElementById("root")); 
