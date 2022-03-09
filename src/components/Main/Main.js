0182f799abacc554b9e7416f8420991556a239fd
AntoFerraro <antodibe_9@hotmail.com>
2022-03-09 20:40:47 -0300

    Ladrillos y NEON

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,12 @@
 import ItemListContainer from "./ItemList/ItemListContainer"
 import ItemDetailContainer from "./ItemDetail/ItemDetailContainer"
+import Welcome from "./WelcomePage/Welcome"
 
 const Main = () => {
        
     return(
-        <main>
+        <main className="background">
+            <Welcome/>
             <h2 className="nuestros_prod">Nuestros Productos</h2>            
             <ItemListContainer/>
             <ItemDetailContainer/>
---
+++ src/components/Main/WelcomePage/Welcome.js
@@ -0,0 +1,12 @@
+import Open from '../../../img/welcome.jpeg';
+
+const Welcome = () => {
+
+    return ( 
+        <div className="div_imagen">
+            <img className="welcomeSign" src={Open} alt={"welcomeSign"}/>
+        </div>
+    )
+}
+
+export default Welcome
